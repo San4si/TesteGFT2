caff00fcc809aaf864780c7375a0ce92a67f0794
San4si <san_juq@hotmail.com>
2021-02-04 16:05:31 -0300

    Teste2

---
+++ compras.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Produto
+{
+    private string nome;
+    private double preco;
+    private int qtd;
+
+    public produto (){
+        
+    }
+}
+
+class compras {
+    static void Main()
+    {
+
+    }
+
+
+}
